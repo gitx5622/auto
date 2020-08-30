6425a55f8812968ff243d01611eb36462860c546
gitx5622 <ggitau5622@gmail.com>
2020-08-31 02:35:39 +0300

    first commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=host=cicd_db;dbname=cicd',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
