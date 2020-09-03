301547c059611d2ab8ca1d6b9118ec5e358684f4
gitx5622 <ggitau5622@gmail.com>
2020-09-03 11:29:36 +0300

    cicd

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@ return [
     'username' => 'bazengadaddy',
     'password' => 'Password123#@!',
     'charset' => 'utf8',
-
+    'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
