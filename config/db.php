319065b7d0db36ffc6d1f02e390fe55c8ce54fba
gitx5622 <ggitau5622@gmail.com>
2020-09-06 08:52:22 +0300

    cicd

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=cicd',
+    'dsn' => 'mysql:host=db;dbname=cicd',
     'username' => 'root',
     'password' => 'Password123#@!',
     'charset' => 'utf8',
