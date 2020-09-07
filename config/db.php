adf7baeb6224d38860c630d1ca5ff95136f37b22
gitx5622 <ggitau5622@gmail.com>
2020-09-07 11:15:11 +0300

    cicd automation

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=cicd',
+    'dsn' => 'mysql:host=172.18.0.3;dbname=cicd',
     // 'dsn' => 'mysql://root:1165@db:3306/cicd',
     'username' => 'root',
     'password' => '1165',
