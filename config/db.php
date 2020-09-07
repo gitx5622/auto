5f81011fab5bd841770c0f4c4078ff6e22bdb782
gitx5622 <ggitau5622@gmail.com>
2020-09-07 11:06:49 +0300

    cicd automation

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=127.0.0.1;dbname=cicd',
-    'dsn' => 'mysql://root:1165@db:3306/cicd',
-//    'username' => 'root',
-//    'password' => 'Password123#@!',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=cicd',
+    // 'dsn' => 'mysql://root:1165@db:3306/cicd',
+    'username' => 'root',
+    'password' => '1165',
     'charset' => 'utf8',
     'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
     // Schema cache options (for production environment)
